a23bb82b7590cc39bad2a4611f2dd88abf447c3a
RaimisM <raimondasmicc@gmail.com>
2025-03-21 10:47:59 +0200

    feat: add Tailwind CSS configuration and styles for improved UI

--- habits/tailwind.config.js
+++ habits/tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}", // Ensure Tailwind scans all Vue files
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
